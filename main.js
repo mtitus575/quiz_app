6ebb2044fbcf1778184cb6b7c70677de1716f2e4
Martin <mjtitus06@gmail.com>
2025-07-05 18:12:11 +0100

    Added JS logic to create all the cards. This is reusable and adapts to the length of the question bank. Eac card has a section for answers (buttons) - only one button per card can be selected.

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ import questionBank from "./questions.js";
 
 const container = document.querySelector(".questionCard");
 
+//Looping over the question bank array of objects.
+questionBank.forEach((set) => {
+  //`set` = each set of questions object.
+  cardStyle(set); //calling the card styling for each question
+});
+
 //Main card
 function cardStyle(set) {
   //function to style the card
@@ -14,14 +20,14 @@ function cardStyle(set) {
   card.append(answersDiv(set));
   card.append(nextBtn());
 }
-
+// This displays the question into the cards
 function questionHeading(set) {
   const questionBnr = document.createElement("h4");
   questionBnr.innerText = set.question;
 
   return questionBnr;
 }
-
+//This is the div containing all the answer buttons per card
 function answersDiv(set) {
   const answerCtn = document.createElement("section");
   answerCtn.className = "answerCtn";
@@ -31,10 +37,27 @@ function answersDiv(set) {
     const answerBtn = document.createElement("button");
     answerBtn.innerText = option;
     answerCtn.append(answerBtn);
+    console.log(option);
+
+    //Change clr based on the answer clicked (green = right, red = wrong)
+    function handleAnswerClick(event) {
+      event.preventDefault();
+
+      if (set.answer === answerBtn.innerText) {
+        answerBtn.style.backgroundColor = "green";
+      } else {
+        answerBtn.style.backgroundColor = "red";
+      }
+
+      const allCardBtns = answerCtn.querySelectorAll("button"); //This selects on buttons from the `answerCtn` only
+      allCardBtns.forEach((button) => (button.disabled = true)); //This line disabled the buttons after 1 selection is made.
+    }
+    answerBtn.addEventListener("click", handleAnswerClick);
+    //I want to stop the user from being able to click more than one option
   }
   return answerCtn;
 }
-
+//This is the next button for each card.
 function nextBtn() {
   const next = document.createElement("button");
   next.innerText = "Next Question =>";
@@ -43,7 +66,10 @@ function nextBtn() {
   return next;
 }
 
-questionBank.forEach((set) => {
-  //`set` = each set of questions object.
-  cardStyle(set); //calling the card styling for each question
-});
+/*TODO Next
+1. Hide all, but one, card.
+2. Allow next button to show the next card
+3. Track the correct answers
+4. Display final score out of all questions
+5. Allow restart
+*/
